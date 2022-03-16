dc99dd17b9a3f0b7a770cc67d78fb18546ac7b23
Andy Ngo <amngo96@gmail.com>
2022-03-16 03:27:05 -0700

    fixed imports

--- components/Movie/index.ts
+++ components/Movie/index.ts
@@ -1,7 +1,6 @@
 import Cast from './Cast';
 import Info from './Info';
 import Movie from './Movie';
-import MovieImages from './MovieImages';
 import SimilarMovies from './SimilarMovies';
 
-export { Cast, Info, SimilarMovies, Movie, MovieImages };
+export { Cast, Info, SimilarMovies, Movie };
